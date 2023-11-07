14db259a24416c369e7993e0e94735c73e791d90
Susanreefman <74648927+Susanreefman@users.noreply.github.com>
2023-11-07 11:37:14 +0100

    Getting weather data

--- GetWeatherData/get_weather_data.py
+++ GetWeatherData/get_weather_data.py
@@ -26,14 +26,14 @@ configure()
 api_key = os.getenv('api_key')
 
 # Start date in unix timestamp
-date = 1644048000
+date = 1644930000
 
 # 1-1-2022
 date# = 1640991600
 
 flat_data_list = []
 
-for i in range(150):
+for i in range(850):
 
     api_url = f'https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=44.64055556&lon=7.60750000&dt={date}&units=metric&appid={api_key}'
     date += 3600
@@ -72,7 +72,7 @@ for i in range(150):
 response.close()
 
 # Define path of output CSV file 
-csv_file = 'C:/Users/Susan/Documents/Datasets/weather_data_Guibergia1.csv' 
+csv_file = 'C:/Users/Susan/Documents/Datasets/weather_data_Guibergia_feb.csv' 
 
 # Write the data in the list to the CSV file
 with open(csv_file, 'w', newline='') as file:
